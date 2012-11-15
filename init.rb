057b91f272fb454c5575a2835522c3a972543296
reubenmallaby <reuben.mallaby@gmail.com>
2012-11-15 12:44:15 +0100

    Initial commit for Bootstrap theme

---
+++ init.rb
@@ -0,0 +1,11 @@
+require 'bootstrap_hooks'
+
+Redmine::Plugin.register :redmine_bootstrap do
+  name 'Redmine Bootstrap theme plugin'
+  author 'Reuben Mallaby'
+  description 'This is a plugin modifying Redmine to use a Bootstrap theme'
+  version '0.0.1'
+  url 'http://mallaby.me/projects/redmine_bootrap'
+  author_url 'http://mallaby.me'
+   
+end
---
+++ lib/bootstrap_hooks.rb
@@ -0,0 +1,4 @@
+class BootstrapHooks < Redmine::Hook::ViewListener
+  render_on(:view_layouts_base_html_head, :partial => 'hooks/redmine_bootstrap/css')
+
+end
