ccde440ff9d2f7ccad0776a3b56d7f88eb825090
reubenmallaby <reuben.mallaby@gmail.com>
2012-11-22 13:03:02 +0100

    Fix for settings-members

--- lib/application_helper_patch.rb
+++ lib/application_helper_patch.rb
@@ -61,7 +61,7 @@ module ApplicationHelper
     pcts[1] = pcts[1] - pcts[0]
     pcts << (100 - pcts[1] - pcts[0])
     #width = options[:width] || '100px;'
-    #legend = options[:legend] || ''
+    legend = options[:legend] || ''
     #content_tag('table',
     #  content_tag('tr',
     #    (pcts[0] > 0 ? content_tag('td', '', :style => "width: #{pcts[0]}%;", :class => 'closed') : ''.html_safe) +
@@ -70,7 +70,7 @@ module ApplicationHelper
     #  ), :class => 'progress', :style => "width: #{width};").html_safe +
     #  content_tag('p', legend, :class => 'pourcent').html_safe
       
-    html << "<div class='progress'>"
+    html << "<div class='progress pourcent' title='#{legend} #{pcts[0]}%'>"
     html << "<div class='bar bar-success' style='width: #{pcts[0]}%;'></div>"
     html << "<div class='bar bar-warning' style='width: #{pcts[1]}%;'></div>"
     html << "<div class='bar bar-danger' style='width: #{pcts[2]}%;'></div>"
