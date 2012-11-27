26b728d300a43cdbb2ce77f04935450ad84c9d6c
reubenmallaby <reuben.mallaby@gmail.com>
2012-11-27 10:37:51 +0100

    Updated version to 0.0.3

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_bootstrap do
   name 'Redmine Bootstrap theme plugin'
   author 'Reuben Mallaby'
   description 'This is a plugin modifying Redmine to use a Bootstrap theme'
-  version '0.0.2'
+  version '0.0.3'
   url 'http://mallaby.me/projects/redmine_bootstrap'
   author_url 'http://mallaby.me'
    
